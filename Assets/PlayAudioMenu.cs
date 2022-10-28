df8cd8eb776876996fdaace788933a6176c6243b
Gabriel <gbtatu@hotmail.com>
2022-10-27 23:38:31 -0300

    icons for menu

--- Assets/PlayAudioMenu.cs
+++ Assets/PlayAudioMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayAudioMenu : MonoBehaviour
@@ -8,9 +9,23 @@ public class PlayAudioMenu : MonoBehaviour
     public AudioClip clipThree;
     private AudioSource source;
 
-    public void PlayAudio() {
-        source = GetComponent<AudioSource>();
-        source.PlayOneShot(clipThree);
+    public Button button;
+    public Sprite playImage;
+    public Sprite pauseImage;
 
+    public void PlayAudio() {
+
+        source = GetComponent<AudioSource>();
+        if (source.loop == false) {
+            source.clip = clipThree;
+            source.loop = true;
+            button.GetComponent<Image>().sprite = pauseImage;
+            source.Play();
+        }
+        else {
+            source.loop = false;
+            button.GetComponent<Image>().sprite = playImage;
+            source.Stop();
+        }
     }
 }
